7f08832c8af3b65e4b89aff9659fc733d71b6550
s.kravtsov <s.kravtsov@hotmail.com>
2026-08-30 14:47:02 +0300

    Add filters, ordering and search for Transactions API

---
+++ finance/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+
+from finance.models import Transaction
+
+
+class TransactionFilter(django_filters.FilterSet):
+    date_from = django_filters.DateFilter(field_name="date", lookup_expr="gte")
+    date_to = django_filters.DateFilter(field_name="date", lookup_expr="lte")
+
+    class Meta:
+        model = Transaction
+        fields = ("type", "currency", "date_from", "date_to")
--- finance/views/admin/transaction.py
+++ finance/views/admin/transaction.py
@@ -2,6 +2,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import DjangoModelPermissions, IsAdminUser
 
+from finance.filters import TransactionFilter
 from finance.models import Transaction
 from finance.serializers.admin.transaction import TransactionAdminSerializer
 
@@ -16,3 +17,13 @@ class TransactionAdminViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
     permission_classes = (IsAdminUser, DjangoModelPermissions)
     serializer_class = TransactionAdminSerializer
     queryset = Transaction.objects.select_related("account", "currency", "lesson")
+    filterset_class = TransactionFilter
+    search_fields = (
+        "comment",
+        "account__student_profile__user__first_name",
+        "account__student_profile__user__last_name",
+        "account__teacher_profile__user__first_name",
+        "account__teacher_profile__user__last_name",
+        "account__company__name",
+    )
+    ordering_fields = ("date", "type", "currency__code")
